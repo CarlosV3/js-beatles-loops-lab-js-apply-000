689d1f6c1887b068bf6d8d6c31a42d7bba4ab1a1
Carlos Vallejo <carlos.v3@outlook.com>
2018-08-08 00:31:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var allMusicians = [];
 for (var m = 0; m<musicians.length; m++) {
-  allMusicians.push(musicians[m] + "plays " + instruments[m]);
+  allMusicians.push(musicians[m] + " plays " + instruments[m]);
     }
   return allMusicians;
 }
