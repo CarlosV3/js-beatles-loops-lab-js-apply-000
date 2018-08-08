910e7bf7110f00450e619cced17f237cc549b3a4
Carlos Vallejo <carlos.v3@outlook.com>
2018-08-08 00:59:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,3 @@ function johnLennonFacts(facts) {
 }
 
 
-function iLoveTheBeatles(number) {
-  var array = [];
-  do {
-    array.push("I love the Beatles!");
-    number++;
-  } while (number < 15);
-  return array;
-}
