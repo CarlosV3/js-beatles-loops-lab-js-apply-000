268af893c60b01ef56246ddd79ea9eda156e3714
Carlos Vallejo <carlos.v3@outlook.com>
2018-08-08 00:33:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,8 @@ for (var m = 0; m<musicians.length; m++) {
 
 
 function johnLennonFacts() {
-  var facts = []
+  var facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
 }
