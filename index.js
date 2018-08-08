502b590fb6723642c1e19185b392cdec3edea3c6
Carlos Vallejo <carlos.v3@outlook.com>
2018-08-08 00:25:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function theBeatlesPlay(musicians,instruments) {
   var allMusicians = [];
 for (var m = 0; m<musicians.length; m++) {
   allMusicians.push(musicians[m] + "plays " + instruments[i]);
-    return allMusicians;
-}
-  
+    }
+  return allMusicians;
 }
 // add solution here
