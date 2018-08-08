68b555bb6a4cc777481f5b7da357e0c6b6992259
Carlos Vallejo <carlos.v3@outlook.com>
2018-08-08 00:40:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ for (var m = 0; m<musicians.length; m++) {
 
 
 function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
-  var excitingFacts = [];
-  var f = 0;
-  while (f<facts.length) {
-    excitingFacts.push(facts[f]+"!!!");
-    f++;
-  }
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
 }
